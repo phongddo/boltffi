72012526eb078aeee78de062c21cc2e6a8030f83
Ali <ali94_200970@yahoo.com>
2025-07-05 03:52:00 +0200

    copy_bytes uses status

--- mobiFFI_core/src/lib.rs
+++ mobiFFI_core/src/lib.rs
@@ -34,3 +34,25 @@ pub extern "C" fn mffi_free_buf_u8(buf: FfiBuf<u8>) {
 pub extern "C" fn mffi_free_string(string: FfiString) {
     drop(string);
 }
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn mffi_copy_bytes(
+    src: *const u8,
+    src_len: usize,
+    dst: *mut u8,
+    dst_cap: usize,
+    written: *mut usize,
+) -> FfiStatus {
+    if src.is_null() || dst.is_null() || written.is_null() {
+        return FfiStatus::NULL_POINTER;
+    }
+
+    if src_len > dst_cap {
+        return FfiStatus::BUFFER_TOO_SMALL;
+    }
+
+    core::ptr::copy_nonoverlapping(src, dst, src_len);
+    *written = src_len;
+
+    FfiStatus::OK
+}
